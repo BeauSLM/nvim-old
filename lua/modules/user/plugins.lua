bc5638ca8097e529b0cf3bd117db6eacd24d12ef
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-06 09:48:28 -0700

    Move lightline to user plugins

--- lua/modules/user/plugins.lua
+++ lua/modules/user/plugins.lua
@@ -1,4 +1,23 @@
 local user = {}
 
+user["itchyny/lightline.vim"] = {
+  event = "UIEnter",
+  config = function ()
+    vim.cmd([[
+    let g:lightline = {
+      \ 'active': {
+      \   'left': [ [ 'mode', 'paste' ],
+      \             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
+      \ },
+      \ 'component_function': {
+      \   'gitbranch': 'FugitiveHead'
+      \ },
+      \ }
+
+    ]])
+  end,
+  opt = true
+}
+
 -- your plugin config
 return user
