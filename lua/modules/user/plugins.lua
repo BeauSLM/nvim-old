1b8337766a8da7ce19444fa85a3112bb7f25192b
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-06 10:39:32 -0700

    Fugitive :GBrowse for github and gitlab

--- lua/modules/user/plugins.lua
+++ lua/modules/user/plugins.lua
@@ -5,6 +5,16 @@ user["junegunn/gv.vim"] = {
   requires = {"tpope/vim-fugitive", opt = true,},
 }
 
+user["tpope/vim-rhubarb"] = {
+  opt = true,
+  after = "vim-fugitive"
+}
+
+user["shumphrey/fugitive-gitlab.vim"] = {
+  opt = true,
+  after = "vim-fugitive"
+}
+
 user["itchyny/lightline.vim"] = {
   event = "UIEnter",
   config = function ()
