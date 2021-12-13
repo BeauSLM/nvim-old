9841b151df4c5f0ac75425478b61284be0507b30
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-13 14:57:45 -0700

    Move off tab completion
    - To cycle through autocomplete entries, use arrow keys or <c-n> and <c-p>.
    
    To cycle through the snippet "typing spots", <c-k> and <c-j>

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -6,35 +6,8 @@ function config.nvim_lsp()
   lspclient.setup()
 end
 
-local function is_prior_char_whitespace()
-  local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
-end
-
 function config.nvim_cmp()
   local cmp = require('cmp')
-
-  local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  end
-  local luasnip = require("luasnip")
-  local function tab(fallback)
-    if cmp.visible() then
-      cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
-      luasnip.expand_or_jump()
-    elseif has_words_before() then
-      cmp.complete()
-    else
-      -- F("<Tab>")
-      fallback()
-    end
-  end
 
   if load_coq() then
     local sources = {}
@@ -43,14 +16,6 @@ function config.nvim_cmp()
   end
   -- print("cmp setup")
   local comp_kind = nil
-  local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
-  local check_back_space = function()
-    local col = vim.fn.col '.' - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
-  end
-
   local sources = {
     {name = 'nvim_lsp'}, {name = 'luasnip'}, {name = 'treesitter', keyword_length = 2},
     {name = 'look', keyword_length = 4}
@@ -118,29 +83,6 @@ function config.nvim_cmp()
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Replace, select = true}),
-      -- ['<Tab>'] = cmp.mapping(tab, {'i', 's'}),
-
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, {"i", "s"}),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {"i", "s"})
-
     },
 
     -- You should specify your *installed* sources.
@@ -173,7 +115,13 @@ function config.luasnip()
 
   vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
   vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
-
+  vim.cmd [[
+    imap <silent><expr> <c-k> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<c-k>'
+    inoremap <silent> <c-j> <cmd>lua require('luasnip').jump(-1)<CR>
+    imap <silent><expr> <C-l> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-l>'
+    snoremap <silent> <c-k> <cmd>lua require('luasnip').jump(1)<CR>
+    snoremap <silent> <c-j> <cmd>lua require('luasnip').jump(-1)<CR>
+  ]]
 end
 
 function config.telescope_preload()
