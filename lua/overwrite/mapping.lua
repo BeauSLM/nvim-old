a3bdf5ff43b4d604e829d5f3aeddc1db9e09f035
Beau McCartney <mccartney.beausl@gmail.com>
2021-12-13 21:38:31 -0700

    Remove unneeded function

--- lua/overwrite/mapping.lua
+++ lua/overwrite/mapping.lua
@@ -6,14 +6,6 @@ local map_args = bind.map_args
 
 local loader = require"packer".loader
 K = {}
-local function check_back_space()
-  local col = vim.fn.col(".") - 1
-  if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-    return true
-  else
-    return false
-  end
-end
 
 local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
