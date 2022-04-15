10c80e6b9eaf78c9cda3aae59b16c1b40749f4dd
Beau McCartney <mccartney.beausl@gmail.com>
2022-04-14 21:33:09 -0600

    Purge ui module

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -17,43 +17,9 @@ ui["windwp/windline.nvim"] = {
 
 ui["lambdalisue/glyph-palette.vim"] = {}
 
-ui["akinsho/bufferline.nvim"] = {
-  disable = true,
-  config = conf.nvim_bufferline,
-  event = "UIEnter",
-  diagnostics_update_in_insert = false,
-  -- after = {"aurora"}
-  -- requires = {'kyazdani42/nvim-web-devicons'}
-  opt = true,
-}
--- 'luaromgrk/barbar.nvim'
--- ui['romgrk/barbar.nvim'] = {
---   config = conf.barbar,
---   requires = {'kyazdani42/nvim-web-devicons'}
--- }
---
--- not so useful...
--- ui["wfxr/minimap.vim"] = {
---   run = ":!cargo install --locked code-minimap",
---   keys = {"<F14>"},
---   cmd = {"Minimap", "MinimapToggle"},
---   setup = conf.minimap
--- }
-
-ui["kyazdani42/nvim-tree.lua"] = {
-  disable = true,
-  cmd = {"NvimTreeToggle", "NvimTreeOpen"},
-  -- requires = {'kyazdani42/nvim-web-devicons'},
-  setup = conf.nvim_tree_setup,
-  config = conf.nvim_tree,
-}
-
 ui["lukas-reineke/indent-blankline.nvim"] = { opt = true, config = conf.blankline } -- after="nvim-treesitter",
 
 ui["sainnhe/sonokai"] = {opt = true, config = conf.sonokai}
 ui["sainnhe/gruvbox-material"] = {opt = true, config = conf.gruvbox}
-
--- cant config cursor line
--- ui["rafamadriz/neon"] = {opt = true, config = conf.neon}
 
 return ui
